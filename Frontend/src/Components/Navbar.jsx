e3879cb241d1226c32129c8875b66ea6dab46b44
ybaataoui <baataouiyoussef@gmail.com>
2024-09-03 17:01:34 -0400

    Add Navigation bar

---
+++ Frontend/src/Components/Navbar.jsx
@@ -0,0 +1,24 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+
+function NavBar() {
+    return (
+        <Navbar bg='dark' variant='dark' expand="lg">
+            <Container >
+                <Navbar.Brand href="#home">Dealership-Auto-Car</Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">Link</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+
+export default NavBar;
