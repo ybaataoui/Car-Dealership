3f5c8228948a947846d061daeac5d0caca5fcefe
Youssef <baataouiyoussef@gmail.com>
2024-09-18 22:18:44 -0400

    Add inquiry module

--- Backend/inquiries/models.py
+++ Backend/inquiries/models.py
@@ -3,10 +3,19 @@ from django.contrib.auth.models import User
 
 
 class Inquiry(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    car_id = models.IntegerField()
+    customer_need = models.CharField(max_length=100)
+    car_title = models.CharField(max_length=100)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100)
+    phone = models.CharField(max_length=100)
+    message = models.TextField(blank=True)
+    # user_id = models.IntegerField(blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="inquiries")
 
     def __str__(self) -> str:
-        return self.title
+        return self.email
